2b60a4b748a53b2eb3bbebaff6cfad8b968b0060
Wolfy0906 <106831974+Wolfy0906@users.noreply.github.com>
2025-04-27 21:52:24 -0700

    Restore '4/26/2025, 10:09:31 PM Initial files for MakeCode project'

--- main.ts
+++ main.ts
@@ -1,54 +1,65 @@
 input.onPinPressed(TouchPin.P0, function () {
-    if (time - globalTime > 0) {
-        score += 5
+    if (Math.abs(globalTime - time) > 0) {
+        score += 1
     }
 })
 // insert sick beats here if u want
 input.onButtonPressed(Button.A, function () {
-    if (A_antiExplode == 0) {
-        A_antiExplode += 1
-    }
+    A_antiExplode += 1
 })
-input.onPinPressed(TouchPin.P1, function () {
-    if (time - globalTime > 0) {
-        score += 10
+input.onButtonPressed(Button.B, function () {
+    B_antiExplode += 1
+    if (A_antiExplode < 1 && B_antiExplode == 1) {
+        basic.showNumber(highscore)
     }
+    B_antiExplode = 0
 })
+let B_antiExplode = 0
 let A_antiExplode = 0
 let globalTime = 0
 let time = 0
-radio.setGroup(143)
+let highscore = 0
 // set this to length of 1 attempt
-let ROUNDLENGTH = 3
+let ROUNDLENGTH = 5
 // set this to break time between "A" press and game start
 let PAUSE = 3
-let highscore = 0
+highscore = 0
 time = 0
 let score = 0
 globalTime = 0
 A_antiExplode = 0
+B_antiExplode = 0
 loops.everyInterval(1000, function () {
     globalTime += 1
 })
 basic.forever(function () {
-    if (A_antiExplode == 1) {
+    if (A_antiExplode == 1 && B_antiExplode < 1) {
         time = globalTime + PAUSE
         while (time > globalTime) {
-            radio.sendValue("c", time - globalTime)
+            basic.showNumber(Math.abs(globalTime - time))
         }
-        basic.clearScreen()
-        radio.sendValue("GO", 1)
+        basic.showString("GO")
         time = globalTime + ROUNDLENGTH
         while (time > globalTime) {
-            radio.sendValue("c", time - globalTime)
+            basic.showNumber(Math.abs(globalTime - time))
         }
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+        basic.clearScreen()
+        basic.pause(500)
+        basic.showNumber(score)
+        basic.pause(2000)
+        basic.clearScreen()
         if (score > highscore) {
             highscore = score
         }
-        radio.sendValue("a", score)
-        radio.sendValue("b", highscore)
         score = 0
         A_antiExplode = 0
-        basic.clearScreen()
+        B_antiExplode = 0
     }
 })
