a10904b911a63deababd5c545ca7eec966a3a82d
Wolfy0906 <106831974+Wolfy0906@users.noreply.github.com>
2025-04-27 21:53:11 -0700

    Restore '4/27/2025, 7:02:21 PM HI'

--- main.ts
+++ main.ts
@@ -1,47 +1,48 @@
 input.onPinPressed(TouchPin.P0, function () {
-    if (Math.abs(globalTime - time) > 0) {
-        score += 1
+    if (time - globalTime > 0) {
+        score += 5
     }
 })
 // insert sick beats here if u want
 input.onButtonPressed(Button.A, function () {
-    A_antiExplode += 1
-})
-input.onButtonPressed(Button.B, function () {
-    B_antiExplode += 1
-    if (A_antiExplode < 1 && B_antiExplode == 1) {
-        basic.showNumber(highscore)
+    if (A_antiExplode == 0) {
+        A_antiExplode += 1
     }
-    B_antiExplode = 0
 })
-let B_antiExplode = 0
+input.onPinPressed(TouchPin.P1, function () {
+    if (time - globalTime > 0) {
+        score += 10
+    }
+})
 let A_antiExplode = 0
 let globalTime = 0
 let time = 0
-let highscore = 0
 // set this to length of 1 attempt
-let ROUNDLENGTH = 5
+let ROUNDLENGTH = 10
 // set this to break time between "A" press and game start
 let PAUSE = 3
-highscore = 0
+let highscore = 0
 time = 0
 let score = 0
 globalTime = 0
 A_antiExplode = 0
-B_antiExplode = 0
 loops.everyInterval(1000, function () {
     globalTime += 1
 })
 basic.forever(function () {
-    if (A_antiExplode == 1 && B_antiExplode < 1) {
+    if (A_antiExplode == 1) {
         time = globalTime + PAUSE
         while (time > globalTime) {
-            basic.showNumber(Math.abs(globalTime - time))
+            basic.showNumber(time - globalTime)
         }
+        basic.clearScreen()
         basic.showString("GO")
         time = globalTime + ROUNDLENGTH
         while (time > globalTime) {
-            basic.showNumber(Math.abs(globalTime - time))
+            basic.showNumber(time - globalTime)
+        }
+        if (score > highscore) {
+            highscore = score
         }
         basic.showLeds(`
             # # # # #
@@ -52,14 +53,15 @@ basic.forever(function () {
             `)
         basic.clearScreen()
         basic.pause(500)
+        basic.showString("Score")
         basic.showNumber(score)
         basic.pause(2000)
         basic.clearScreen()
-        if (score > highscore) {
-            highscore = score
-        }
+        basic.showString("HI")
+        basic.showNumber(highscore)
+        basic.pause(2000)
+        basic.clearScreen()
         score = 0
         A_antiExplode = 0
-        B_antiExplode = 0
     }
 })
